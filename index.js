a33666db00f259ed66377ccbcc88450ec9023095
nceeoh7 <51413645+nceeoh7@users.noreply.github.com>
2023-03-28 17:51:43 +0000

    End point

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
+const ejs = require("ejs");
 const path = require("path");
 const app = express();
 
+//Sets our view engine to load files ending in .ejs
+app.set("view engine", "ejs");
 app.use(express.static("public"));
 
-const PORT = 3000;
+const PORT = 5000;
 
 app.listen(PORT, () => {
   console.log("App listening on port ", PORT);
 });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  //res.sendFile(path.resolve(__dirname + "/views/", "index.html"));
+  res.render("index");
 });
 
 app.get("/profile", (req, res) => {
@@ -28,4 +32,8 @@ app.get("/faq", (req, res) => {
 
 app.get("/terms", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/", "terms.html"));
+});
+
+app.get("/slides", (req, res) => {
+  res.sendFile(path.resolve(__dirname + "/views/", "slide-show.html"));
 });
